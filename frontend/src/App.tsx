3225323d20c98883d17d0e3a63b1b2eb3450487b
koalacapek <hanskr7882@gmail.com>
2025-05-25 21:30:06 +1000

    remove comment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,3 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css"
 import Navbar from "./components/Navbar"
 import Dashboard from "./components/Dashboard"
@@ -9,7 +6,6 @@ function App() {
   return (
     <div className="font-open-sans w-full h-fit bg-[#F5F5F5]">
       <Navbar />
-      {/* Content */}
       <Dashboard />
     </div>
   )
